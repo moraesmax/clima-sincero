e824e28df980fbfc3bc0976714fe1303b17979ae
Maximiliano Moraes <moraes.maximiliano@gmail.com>
2016-04-30 17:00:16 -0300

    v1.0

---
+++ gulpfile.js
@@ -0,0 +1,310 @@
+"use strict";
+
+// Módulos (Requires)
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var p = require('gulp-load-plugins')();
+
+// //// Utilitários
+var path = require('path');
+var rimraf = require('rimraf');
+var pngquant = require('imagemin-pngquant');
+var runSequence = require('run-sequence');
+
+// Ambientes
+var dev = p.environments.development;
+var prd = p.environments.production;
+
+// Constantes
+var PASTA_DEV = 'dev/'
+var PASTA_DEST = 'res/'
+
+// Variáveis públicas
+var banner = ""
+var caminhos = {}
+var pkg;
+var worker;
+
+// Configurações do Package (projeto)
+pkg = require('./package.json')
+
+/*
+ * Configurações
+ */
+
+// TODO: Criar um switch para usar como App (pasta app) ou Web (pastas CSS e JS) no dev.
+
+// Seta os caminhos padrão
+caminhos = {
+	"css": {
+		"origem": path.join(PASTA_DEV, 'app/**/*.scss'),
+		"destino": path.join(PASTA_DEST, 'app/')
+	},
+
+	"js": {
+		"origem": path.join(PASTA_DEV, 'app/**/*.js'),
+		"destino": path.join(PASTA_DEST, 'app/')
+	},
+
+	"hogan": {
+		"origem": path.join(PASTA_DEV, 'app/**/*.hogan'),
+		"destino": path.join(PASTA_DEST, 'app/')
+	},
+
+	"img": {
+		"origem": path.join(PASTA_DEV, 'img/**/*'),
+		"destino": path.join(PASTA_DEST, 'img/')
+	},
+
+	"libs": {
+		"origem": path.join(PASTA_DEV, 'lib/**/*.!(md|txt|html|json)'),
+		"destino": path.join(PASTA_DEST, 'lib/')
+	},
+
+	"dist": {
+		"origem": 'dist/',
+	}
+}
+
+// Banner (meta)
+banner = ['',''
+].join('\n');
+
+// Gerenciador de Erros
+function handleError(err) {
+  console.log(err);
+  this.emit('end');
+}
+
+/*
+ * CSS
+ */
+
+//TODO: Output (console)
+
+gulp.task('css', function(event) {
+	return gulp.src(caminhos.css.origem)
+		.pipe(p.plumber({ errorHandler: handleError }))
+		// Pré-processamento
+		.pipe(p.sass())
+		.pipe(prd(p.shorthand()))
+		.pipe(p.pleeease({"minifier": prd(), "autoprefixer": {browsers: ['last 2 versions', 'ie 9', '> 1%']}}))
+		.pipe(prd(p.csso()))
+		// Cabeçalho
+		.pipe(p.header(banner, pkg))
+		// Minificar e otimizar
+		.pipe(p.rename({extname: ".min.css"}))
+		.pipe(prd(p.size()))
+		.pipe(gulp.dest(caminhos.css.destino))
+		//
+		.pipe(dev(p.livereload()))
+});
+
+/*
+ * Scripts (JS)
+ */
+
+//TODO: Output (Console)
+gulp.task('js', function(event) {
+	return gulp.src(caminhos.js.origem)
+		.pipe(p.plumber({ errorHandler: handleError }))
+		// JSHint
+		.pipe(dev(p.jshint({"asi":true})))
+		.pipe(dev(p.jshint.reporter('default')))
+		// Concatena arquivos
+		.pipe(p.concat('frmnt.js'))
+		// Minificador
+		.pipe(prd(p.uglify()))
+		// Header
+		.pipe(p.header(banner, pkg))
+		// Saída
+		.pipe(gulp.dest(caminhos.js.destino))
+		// Atualizar Navegador
+		.pipe(dev(p.livereload()));
+
+	return;
+})
+
+/*
+ * Hogan
+ */
+
+//TODO: Output (Console)
+
+gulp.task('hogan', function(event) {
+	return gulp.src(caminhos.hogan.origem)
+		.pipe(p.plumber({ errorHandler: handleError }))
+		// Compilador
+		.pipe(p.hoganPrecompile())
+		//.pipe(p.defineModule('plain'))
+		.pipe(p.declare({namespace: 'App.Views', noRedeclare: true}))
+		// Concatena arquivos
+		.pipe(p.concat('views.js'))
+		// Minificador
+		.pipe(prd(p.uglify()))
+		// Header
+		.pipe(p.header(banner, pkg))
+		// Saída
+		.pipe(gulp.dest(caminhos.hogan.destino))
+		// Atualizar Navegador
+		.pipe(dev(p.livereload()));
+})
+
+/*
+ * Imagens
+ */
+
+//TODO: Kraken.io
+//TODO: Sync (on Delete)
+gulp.task('img', function(a,b,c) {
+	return gulp.src(caminhos.img.origem)
+		.pipe(p.plumber({ errorHandler: handleError }))
+		// Minifica
+		.pipe(p.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true, verbose: true, use: [pngquant()] }))
+		// Saída
+		.pipe(gulp.dest(caminhos.img.destino))
+		// Atualizar Navegador
+		.pipe(dev(p.livereload()));
+})
+
+/*
+ * Incrementa versão
+ */
+gulp.task('ver', function(){
+	// Incrementa versão
+	gulp.src('./package.json')
+    .pipe(p.bump({type: 'minor'}))
+    .pipe(gulp.dest('./'))
+});
+
+/*
+ * Libs (JS)
+ */
+
+//TODO: Output (Console)
+
+gulp.task('libs', function(event) {
+	return gulp.src(caminhos.libs.origem)
+		// Saída
+		.pipe(gulp.dest(caminhos.libs.destino))
+})
+
+/*
+ * Inicializa Pasta
+ */
+
+//TODO: Output (Console)
+
+gulp.task('initDir', function(event) {
+	var mkdirp = require('mkdirp');
+	var globParent = require('glob-parent');
+
+	for (let key in caminhos) {
+		mkdirp(globParent(caminhos[key].origem));
+	}
+
+	return;
+});
+
+/*
+ * Gera output
+ */
+
+//TODO: Output (Console)
+
+gulp.task('dist', function(event) {
+	// Limpa pasta de deploy
+	gutil.log("Limpando /dist");
+	rimraf('/dist/**', function (er) {
+    // Seleciona arquivos
+		gutil.log("Gerando /dist");
+		gulp.src([
+				'**/*',
+				'!package.json',
+				'!gulpfile.js',
+				'!bower.json',
+				'!**/.*',
+				'!**/dev',
+				'!**/dev/**',
+				'!**/node_modules',
+				'!**/node_modules/**',
+				'!**/dist',
+				'!**/dist/**'])
+	    .pipe(p.using())
+	    .pipe(gulp.dest(caminhos.dist.origem))
+  });
+
+	return;
+})
+
+/**
+ ** Watch
+ **/
+gulp.task('watch', function() {
+	// Iniciar LiveReload
+	p.livereload.listen();
+
+	// Iniciarlizar pastas
+	gulp.start('initDir');
+
+	// Watches
+	gulp.watch(caminhos.css.origem, ['css']);
+	gulp.watch(caminhos.js.origem, ['js']);
+	gulp.watch(caminhos.libs.origem, ['libs']);
+	gulp.watch(caminhos.hogan.origem, ['hogan']);
+
+	// Arquivos .html/.php
+	gulp.watch(['./**/*.php','./**/*.html'], (e)=>{
+		gulp.src(e.path)
+		.pipe(p.livereload());
+		return;
+	});
+
+	// Imagens
+	var imgs = gulp.watch(caminhos.img.origem);
+	imgs.on('change', function(event){
+		// type: deleted, changed, added, path: filename
+		switch(event.type) {
+			case "added":
+			case "changed":
+			case "renamed":
+				gutil.log('Minificando ' + event.path);
+				gulp.src(event.path)
+					.pipe(p.plumber({ errorHandler: handleError }))
+					// Minifica
+					.pipe(p.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true, verbose: true, use: [pngquant()] }))
+					// Saída
+					.pipe(gulp.dest(caminhos.img.destino))
+					// Atualizar Navegador
+					.pipe(dev(p.livereload()));
+				break;
+
+			case "deleted":
+				console.log("del.sync");
+				break;
+		}
+	});
+
+});
+
+/*
+ * Preparar para deploy
+ */
+gulp.task('build', function(){
+	// Seta Producao
+	p.environments.current(prd);
+
+	// Log
+	gutil.log('Gerando Produção');
+
+	// Tarefas
+	runSequence(
+		'ver',
+		['css','js','libs','hogan'],
+		'dist');
+	// Tarefas
+});
+
+
+// Define a tarefa padrão
+gulp.task('default', ['watch']);
---
+++ res/app/frmnt.js
@@ -0,0 +1,223 @@
+
+var colorCombination = [{
+        backgroundColor: "#2F343A",
+        textColor: "#fff",
+        highlightColor: "#117DEC",
+        temperatureColor: "#3B4044"
+    }, {
+        backgroundColor: "#2F343A",
+        textColor: "#fff",
+        highlightColor: "#F60400",
+        temperatureColor: "#3B4044"
+    }, {
+        backgroundColor: "#14A77A",
+        textColor: "#fff",
+        highlightColor: "#B2F5E4",
+        temperatureColor: "#B2F5E4"
+    }, {
+        backgroundColor: "#E7152E",
+        textColor: "#fff",
+        highlightColor: "#FCC701",
+        temperatureColor: "#CF273A"
+    },
+    {
+        backgroundColor: "#193565",
+        textColor: "#F9E4CE",
+        highlightColor: "#FCE36F",
+        temperatureColor: "#F9E4CE"
+    }
+];
+
+var messages = {
+    "tempestade": ["Fucking <b>fúria dos trovões</b> do inferno!"],
+    "chuvisco": ["Tá pingando igual <b>meu pau</b> depois de mijar."],
+    "chuva": ["Puta que pariu! Tá <b>chovendo<b> nessa merda!"],
+    "neve": ["Chegou o <b>papai noel</b> filho da puta com essa neve do caralho"],
+    "nevoa": ["Não dá pra ver <b>bosta</b> nenhuma nessa porra!"],
+    "fumaca": ["Alguém acendeu um <b>baseado gigante</b>? Tá só fumaça!"],
+    "areia": ["Mas que <b>puta que pariu</b> de ar é esse?"],
+    "cinza vulcanica": ["Olha <b>pompéia</b> aí caralho! Vulcão filho da puta!"],
+    "tornado": ["Enfia o <b>tornado</b> no cu seu clima arrombado!"],
+    "limpo": ["O dia tá bonito, vai pra rua levar <b>merda de passarinho</b> na cara ou algo do tipo."],
+    "nuvem": ["Tem umas nuvens <b>lokas</b> no céu, deus deve tá fumando um baseado maroto."],
+    "apocalipse": ["Corre pra tentar salvar essa <b>bosta</b> que tu chama de vida! é o <b>apocalipse</b>!"],
+    "outros": ["Porra, <b>vai se fuder</b>, quer saber o tempo? olha pela janela seu filho da puta!"]
+}
+
+var temperatures = {
+    "inferno": ["Eu acabei de ver um urubu pegando fogo enquanto voava!"],
+    "muito quente": ["Quente pra caralho, tá parecendo um forno essa merda!"],
+    "quente": ["Tá curtindo o calor, satanás?"],
+    "pouco quente": ["Aproveite o dia com os pássaros, as abelhas e essas merdas."],
+    "ameno": ["Hora de fazer um piquenique de filho da puta"],
+    "tranquilo": ["Bota o cu pro ar e aproveita o clima aê caralho"],
+    "meio termo": ["Tá começando a ficar frio nessa buceta"],
+    "frio": ["Quem foi o animal que abriu a geladeira?"],
+    "muito frio": ["Puta que pariu, tá frio pra porra!"],
+    "frio demais": ["Caralho! Meu cu ta congelando!"],
+    "frio pra caramba": ["Puta que pariu! Tá me achando com cara de pinguim?"],
+    "congelando": ["Caralho, polo norte filho de uma puta de merda do cacete!!"]
+}
+
+function messageForTemperature(t) {
+    if (t >= 38) {
+        return temperatures["inferno"];
+    }
+    if (t < 38 && t >= 32) {
+        return temperatures["muito quente"];
+    }
+    if (t < 32 && t >= 27) {
+        return temperatures["quente"];
+    }
+    if (t < 27 && t >= 23) {
+        return temperatures["pouco quente"];
+    }
+    if (t < 23 && t >= 21) {
+        return temperatures["ameno"];
+    }
+    if (t < 21 && t >= 16) {
+        return temperatures["tranquilo"];
+    }
+    if (t < 16 && t >= 10) {
+        return temperatures["meio termo"];
+    }
+    if (t < 10 && t >= 4) {
+        return temperatures["frio"];
+    }
+    if (t < 4 && t >= -3) {
+        return temperatures["muito frio"];
+    }
+    if (t >= -6 && t < -3) {
+        return temperatures["frio demais"];
+    }
+    if (t < -6 && t >= -12) {
+        return temperatures["frio pra caramba"];
+    }
+    if (t < 12) {
+        return temperatures["congelando"];
+    }
+}
+
+function messageForWeather(code) {
+    if (code >= 200 && code < 300) {
+        return messages["tempestade"];
+    }
+    if (code >= 300 && code < 400) {
+        return messages["chuvisco"];
+    }
+    if (code >= 400 && code < 500) {
+        return messages["chuva"];
+    }
+    if (code >= 500 && code < 600) {
+        return messages["neve"];
+    }
+    if (code == 701 || code == 721 || code == 741) {
+        return messages["nevoa"];
+    }
+    if (code == 711) {
+        return messages["fumaca"];
+    }
+    if (code == 731 || code == 751 || code == 761 || code == 771) {
+        return messages["areia"];
+    }
+    if (code == 762) {
+        return messages["cinza vulcanica"];
+    }
+    if (code == 781) {
+        return messages["tornado"];
+    }
+    if (code == 800) {
+        return messages["limpo"];
+    }
+    if (code >= 801 && code < 900) {
+        return messages["nuvem"];
+    }
+    if (code >= 900 && code <= 906) {
+        return messages["apocalipse"];
+    }
+    return messages["outros"];
+}
+
+function logoForWeather(code) {
+    if (code >= 200 && code < 300) {
+        return "wi wi-thunderstorm";
+    }
+    if (code >= 300 && code < 400) {
+        return "wi wi-sprinkle";
+    }
+    if (code >= 500 && code < 600) {
+        return "wi wi-snow";
+    }
+    if (code == 701 || code == 721 || code == 741) {
+        return "wi wi-fog";
+    }
+    if (code == 711) {
+        return "wi wi-smoke"
+    }
+    if (code == 731 || code == 751 || code == 761 || code == 771) {
+        return "wi wi-smog";
+    }
+    if (code == 762) {
+        return "wi wi-volcano";
+    }
+    if (code == 781) {
+        return "wi wi-tornado";
+    }
+    if (code == 800) {
+        return "wi wi-day-sunny";
+    }
+    if (code >= 801 && code < 900) {
+        return "wi wi-cloudy";
+    }
+    if (code >= 900 && code <= 906) {
+        return "wi wi-meteor";
+    }
+    return "wi wi-cloud";
+}
+
+
+function randomColorCombination() {
+    return colorCombination[Math.floor(Math.random() * colorCombination.length)];
+}
+
+function success(position) {
+
+    $(".preloader .text").html("Beleza! <br>Te achei, agora espera enquanto eu procuro a porra do clima.")
+
+    $.get("http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&APPID=5785af41e9ed8edc9b4aca0c0dcbc755&units=metric", function(data) {})
+        .done(function(data) {
+            $(".preloader").css("opacity", 0);
+
+            setTimeout(function() {
+                $(".preloader").hide();
+                var layout = randomColorCombination();
+                $("html, body").css("background-color", layout.backgroundColor);
+                $(".graus").html(parseInt(data.main.temp) + "°");
+                $(".name").html(data.name);
+                $(".text .bottom").html(messageForTemperature(parseInt(data.main.temp)));
+                $(".text .top").html(messageForWeather(data.weather[0].id));
+                $(".text .top").css("color", layout.textColor);
+                $(".name").css("color", layout.textColor);
+                $(".text .bottom").css("color", layout.textColor);
+                $(".text .top b").css("color", layout.highlightColor);
+                $(".graus").css("color", layout.temperatureColor);
+                $("i").css("color", layout.textColor);
+                $("i").removeClass().addClass("icon wi " + logoForWeather(data.weather[0].id));
+                console.log(data);
+            }, 3000);
+
+        })
+        .fail(function() {
+            $(".preloader .text").html("Puta que pariu! Deu um erro do caralho aqui! <br>Atualiza a página aí porra!");
+        });
+}
+
+function error() {
+    $(".preloader .text").html("Puta que pariu! Ou essa tua merda de navegador não tem geolocation ou deu um erro do caralho aqui!");
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+} else {
+    error();
+}
